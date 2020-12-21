5bb15195d9834722a22b117c3a3b3f65f708baf5
yuki <myukino1213@gmail.com>
2020-12-21 15:51:17 +0900

    [add]プレゼントボックスを作成

--- unity1week_akeru/Assets/Scenes/Script/Game/MakeBox.cs
+++ unity1week_akeru/Assets/Scenes/Script/Game/MakeBox.cs
@@ -4,16 +4,61 @@ using UnityEngine;
 
 public class MakeBox : MonoBehaviour
 {
-    public GameObject[] box = new GameObject[6];
+    SpriteRenderer MainSpriteRenderer;
+
+    public GameObject PresentBox;
+    public GameObject[] PresentBoxs;
+    public GameObject PresentSenter;
+
+    public SpriteRenderer[] PresentSprite;
+    private Sprite[] RedPatter;
+    private Sprite[] BluePatter;
+    private Sprite[] GreenPatter;
+    private Sprite[] YellowPatter;
+    private Sprite[] PurplePatter;
+    private Sprite[] WhitePatter;
+
+    private Vector3[] PresentPosi = new Vector3[4] 
+    {
+        new Vector3(5, 0),
+        new Vector3(0, 5),
+        new Vector3(-5, 0),
+        new Vector3(0, -5),
+
+    };
 
     // Start is called before the first frame update
     private void Awake()
     {
 
+        RedPatter = Resources.LoadAll<Sprite>("RedSquare");
+        BluePatter = Resources.LoadAll<Sprite>("BlueSquare");
+        GreenPatter = Resources.LoadAll<Sprite>("GreenSquare");
+        YellowPatter = Resources.LoadAll<Sprite>("YellowSquare");
+        PurplePatter = Resources.LoadAll<Sprite>("PurpleSquare");
+        WhitePatter = Resources.LoadAll<Sprite>("WhiteSquare");
+
+        PresentBoxs = new GameObject[4];
+        for(int i = 0; i < PresentBoxs.Length; i ++)
+        {
+            PresentBoxs[i] = PresentBox;
+            PresentBoxs[i] = Instantiate(PresentBoxs[i]);
+            PresentBoxs[i].name = "PresentBoxs" + i;
+            PresentBoxs[i].transform.SetParent(PresentSenter.transform, true);
+            PresentBoxs[i].transform.position = PresentPosi[i];
+        }
+        PresentSprite = new SpriteRenderer[6];
+        for (int i = 0; i < 6; i++)
+        {
+            PresentSprite[i] = PresentBoxs[0].transform.GetChild(i).gameObject.GetComponent<SpriteRenderer>();
+        }
+        PresentSprite[2].sprite = WhitePatter[2];
+        PresentSprite[3].sprite = WhitePatter[3];
+        PresentSprite[4].sprite = WhitePatter[4];
+        PresentSprite[5].sprite = WhitePatter[5];
     }
     void Start()
     {
-        
     }
 
     // Update is called once per frame
